6ad441ac8b72b88471cefbd19a74d29d1dd9afe4
calee14 <caplee1000@gmail.com>
2018-10-20 22:19:43 -0700

    classify image

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from models.object_classifier import Object_Predicter
+import os
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,10 +12,20 @@ configure_uploads(app, photos)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    print(request.files.getlist('photo'))
+	# check if it is the right method
     if request.method == 'POST' and 'photo' in request.files:
+    	# get the photo
         filename = photos.save(request.files['photo'])
-        return filename
+        # init the predicter
+        classifier = Object_Predicter()
+        # predict given image
+        classifier.predict(os.path.join(os.path.dirname(os.path.realpath(__file__)), f'static/img/{filename}'))
+        # retrieve the concepts
+        response = classifier.get_concepts()
+        # remove it since we no longer need it
+        os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), f'static/img/{filename}'))
+        # return the results
+        return ', '.join([f"{concept[0]}{concept[1]}" for concept in response])
     return render_template('upload.html')
 
 @app.route('/', methods=['GET'])
