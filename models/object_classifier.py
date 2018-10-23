199045fae9fc067f96be9166fd7892a5b50589b3
Rafael Cenzano <32753063+SavageCoder77@users.noreply.github.com>
2018-10-23 12:57:47 -0700

    new file

---
+++ models/object_classifier.py
@@ -0,0 +1,28 @@
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+import os
+
+class Object_Predicter():
+	def __init__(self):
+		app = ClarifaiApp(api_key='YOURAPIKEY')
+		self.model = app.models.get('apparel')
+		self.concepts = []
+	def predict(self, link):
+		image = ClImage(file_obj=open(link, 'rb'))
+		response = self.model.predict([image])
+		concepts = response['outputs'][0]['data']['concepts']
+		for concept in concepts:
+		    self.concepts.append([concept['name'], concept['value']])
+
+	def get_concepts(self):
+		# get the list of concepts
+		return self.concepts
+
+	def get_top_concepts(self, top):
+		# return top four results
+		return self.concepts[:top]
+
+# if __name__ == "__main__":
+# 	classifier = Object_Predicter()
+# 	path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../static/img/hi.jpg')
+# 	classifier.predict(path)
